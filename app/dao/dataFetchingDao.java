29ca32cea824de22c5121ffd42c6e9e37cce789c
AnamikaNexxo <57352677+AnamikaNexxo@users.noreply.github.com>
2019-12-18 16:49:16 +0530

    changes made in modal controller and UI

--- app/dao/dataFetchingDao.java
+++ app/dao/dataFetchingDao.java
@@ -3,28 +3,24 @@ package dao;
 
 import io.ebean.SqlRow;
 
-
 import java.util.List;
 
-    public class DataFetchingDao {
-        public static List<SqlRow> getHumidityData()
-        {
-            return DbConnector.createSqlQuery("SELECT HUMIDITY,city_name FROM weather_features WHERE DATE(DT_CL)='2015-01-01'").findList();
-        }
-
-        public static List<SqlRow> getHumidityData()
-        {
-            return DbConnector.createSqlQuery("SELECT pressure,city_name FROM weather_features WHERE DATE(DT_CL)='2015-01-01'").findList();
-        }
-
-        public static List<SqlRow> getHumidityData()
-        {
-            return DbConnector.createSqlQuery("SELECT wind_deg,city_name FROM weather_features WHERE DATE(DT_CL)='2015-01-01'").findList();
-        }
-
+public class DataFetchingDao {
+    public static List<SqlRow> getHumidityData() {
+        return DbConnector.createSqlQuery("SELECT HUMIDITY,city_name FROM weather_features WHERE DATE(DT_CL)='2015-01-01'").findList();
     }
 
+    public static List<SqlRow> getPressureData() {
+        return DbConnector.createSqlQuery("SELECT pressure,city_name FROM weather_features WHERE DATE(DT_CL)='2015-01-01'").findList();
+    }
 
-
+    public static List<SqlRow> getWindDegData() {
+        return DbConnector.createSqlQuery("SELECT wind_deg,city_name FROM weather_features WHERE DATE(DT_CL)='2015-01-01'").findList();
+    }
 
 }
+
+
+
+
+
--- app/dto/BarPlaystore2.java
+++
@@ -1,4 +0,0 @@
-package dto;
-
-public class BarPlaystore2 {
-}
