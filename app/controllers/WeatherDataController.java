3fe44c1ab68efb42e87e87607d7823be277409da
AnamikaNexxo <57352677+AnamikaNexxo@users.noreply.github.com>
2019-12-18 19:49:28 +0530

    created service method and controller

--- app/controllers/WeatherDataController.java
+++ app/controllers/WeatherDataController.java
@@ -3,10 +3,10 @@ package controllers;
 import com.google.inject.Inject;
 import dto.CityHumidityDto;
 import play.data.FormFactory;
-import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Http;
 import play.mvc.Result;
+import service.WeatherDataService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,34 +19,14 @@ public class WeatherDataController extends Controller {
         this.formFactory = formFactory;
     }
 
-//  method 1  chart
-//  to get data from the list and return in json format for chart1
 public Result chart(Http.Request request){
 
-        //TODO: call service to get city pressure DTO
-    List<CityHumidityDto> cityHumidityDtos = new ArrayList<>();
-/*
+    //TODO: call service to get city pressure DTO
+/*  //TODO: BREAK THE FOLLOWING CODE TO dao AND SERVICES*/
 
-//TODO: BREAK THE FOLLOWIN CODE TO dao AND SERVICES
-List<SqlRow> datalist = DbConnector.createSqlQuery("\n" + "SELECT HUMIDITY,city_name FROM weather_features WHERE DT_CL='2015-01-01 00:00:00'").findList();
-    List<CityHumidityDto> cityHumidityDtos = new ArrayList<>();
+List<CityHumidityDto> cityHumidityDtos = new ArrayList<>();
+return ok(WeatherDataService.chartService());
 
-    for(SqlRow row : datalist){
-        CityHumidityDto playstore = new CityHumidityDto();
-        playstore.setCityName(row.getString("Humidity"));
-        playstore.setHumidityValue(row.getBigDecimal("city_name"));
-        cityHumidityDtos.add(playstore);
-    }
-
-    CityPressureDto cityPressureDto = new CityPressureDto();
-
-    for(CityHumidityDto store : cityHumidityDtos){
-        cityPressureDto.getCityNames().add(store.getCityName());
-        cityPressureDto.getPressureValues().add(store.getHumidityValue());
-    }
-
-    Logger.info(Json.toJson(cityPressureDto).toString());*/
-    return ok(Json.toJson(cityHumidityDtos));
 }
 
 
--- app/service/WeatherDataService.java
+++ app/service/WeatherDataService.java
@@ -1,13 +1,32 @@
 package service;
 
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import dao.DataFetchingDao;
+import io.ebean.SqlRow;
+import play.libs.Json;
+
+import java.util.List;
+
 public class WeatherDataService {
 //TODO: below
-method()
-{
 
-    //returning dto
-}
+ public static ObjectNode chartService()
+ {
+         ObjectNode result = Json.newObject();
+         ArrayNode an = result.putArray("aaData");
+         List<SqlRow> rows= DataFetchingDao.getHumidityData();
 
-
+         for(SqlRow rowData:rows)
+         {
+             ObjectNode rowObject= Json.newObject();
+//             rowObject.put("0",rowData.getString("DataField1"));
+//             rowObject.put("1",rowData.getString("DataField1")+ " "+rowData.getString("DataField1"));
+//             rowObject.put("2",rowData.getString("DataField1"));
+//             rowObject.put("3",rowData.getString("DataField1"));
+//             an.add(rowObject);
+         }
+         return result;
+     }
 
 }
